9680e59222e83524aa26d90926bd539f6a9984ea
josef3113 <33261454+josef3113@users.noreply.github.com>
2018-02-25 13:05:49 +0200

    hello word

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApplication1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            HelloWorld();
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            int numberOfItem = 0;
+            Console.WriteLine("Hello world");
+            Console.WriteLine("my number is {0} {1}", numberOfItem, 7);
+            Console.WriteLine("my number is way two num = " + numberOfItem);
         }
     }
 }
